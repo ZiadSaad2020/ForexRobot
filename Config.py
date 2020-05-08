6552ab42db5d17f9907c6481ae9af88de1eeb87c
Ziad Youssef <ziad_youssef@hotmail.fr>
2020-05-07 22:27:11 -0400

    commit test

--- Config.py
+++ Config.py
@@ -3,3 +3,6 @@ api_token = '8dd70803707c12b522138b20065492c4-a90939bb719b1416cdc730932dca0fb9'
 csv_file = 'historicalData.txt'
 
 order_url = 'https://api-fxpractice.oanda.com/v3/accounts/101-002-14506410-001/orders'
+
+ziad = 'test'
+
