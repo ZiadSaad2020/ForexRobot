c356ac2835ab09ca4b3035f5e31c2472aece755a
ZiadSaad2020 <59977808+ZiadSaad2020@users.noreply.github.com>
2020-05-14 10:33:13 -0400

    Update CurrentOrders.py

--- CurrentOrders.py
+++ CurrentOrders.py
@@ -12,7 +12,7 @@ class listAllOrders:
     @staticmethod
     def listOrders():
         get_orders = requests.get(Config.url+listAllOrders.endpoint,
-                                  headers=Config.headers)
+                                  headers=Config.headers) #api token is inside headers variable
         print("Status Code: "+ str(get_orders.status_code))
         print('\nOrders: {}'.format(get_orders.content))
 
